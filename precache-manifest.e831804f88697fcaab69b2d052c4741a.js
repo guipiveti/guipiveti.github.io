089146f96e79655038c6e7767fc682194e52b15d
Guilherme <35786089+guipiveti@users.noreply.github.com>
2020-04-30 21:24:27 -0300

    Updates

---
+++ precache-manifest.e831804f88697fcaab69b2d052c4741a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5819c2009dd80f22bea258a89d63d938",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "27c9250a629eef4830e2",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1fd63edd6f9faadb98c",
+    "url": "/react-gh-pages/static/js/2.09ad2272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.09ad2272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c9250a629eef4830e2",
+    "url": "/react-gh-pages/static/js/main.bcdf618d.chunk.js"
+  },
+  {
+    "revision": "701098fd537e77c2d6fe",
+    "url": "/react-gh-pages/static/js/runtime-main.b10432af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
